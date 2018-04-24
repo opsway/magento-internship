9b9ebfc7de1a6f9e8c07c9c62a73a318c6d39de9
Max Voronov <v0id@list.ru>
2018-04-25 00:02:32 +0200

    Added HelloWorld extension

---
+++ src/app/code/local/OpsWay/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+class OpsWay_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Hello World action
+     */
+    public function indexAction()
+    {
+        echo 'Hello World';
+    }
+}
